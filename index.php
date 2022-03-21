6a504f9babc9e6c9aacebe38313ab853a8030521
Danila <syvs0990@mail.ru>
2022-03-21 21:55:39 +0700

    Created router

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
     require_once("components/head.php");
-    if (!$user_signed) {
-        header("Location: login.php");
-    }
-    
+    if (!$user_signed) header("Location: login.php");   
 ?>
 <div class="main">
     <div class="container">
@@ -21,21 +18,29 @@
                             <div class='main__cell'>Type</div>";
 
                     $users;
-                    $users_query = "SELECT user.id, user.name, user.surname, user.login, user.password, user.birthdate,
-                                    user_type.name AS type, sex.name AS sex FROM `user`
-                                    INNER JOIN user_type ON user_type.id = user.user_type
+                    $entryQuantity  = 20;
+                    $itemsFrom      = $_GET['page'] * $entryQuantity - $entryQuantity;
+                    $users_query    = "SELECT user.id, user.name, user.surname, user.login, user.password, user.birthdate,
+                                    sex.name AS sex, user_type.name AS type FROM `user`
                                     INNER JOIN sex ON sex.id = user.sex
-                                    LIMIT 50";
+                                    INNER JOIN user_type ON user_type.id = user.user_type
+                                    LIMIT $itemsFrom, $entryQuantity";
+
                     if (isset($_COOKIE['login']) && isset($_COOKIE['password'])) {
+
                         $cookie_login       = $_COOKIE['login'];
                         $cookie_password    = $_COOKIE['password'];
                         $users              = $connection->query($users_query);
+
                     } else if (isset($_SESSION['login']) && isset($_SESSION['password'])) {
+
                         $session_login      = $_SESSION['login'];
                         $session_password   = $_SESSION['password'];
                         $users              = $connection->query($users_query);
+
                     }
                     while($row = mysqli_fetch_assoc($users)) {
+
                         $rowCells = "";
                         foreach($row as $key => $value) {
                             $rowCells = $rowCells . "<div class='main__cell'>$value</div>";
@@ -45,6 +50,25 @@
                     }
 
                     echo "</div>";
+
+                    $countUsers = $connection->query("SELECT COUNT(*) AS count FROM `user`");
+                    $countUsers = ceil($countUsers->fetch_assoc()['count'] / $entryQuantity);
+
+                    $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+                    $routes = "<div>$currentPage</div>";
+
+                    for ($i = $currentPage - 1; $i > $currentPage - 3 && $i != 0; $i--) {
+                        $routes = "<a href='?page=$i'>$i</a>" . $routes;
+                    }
+
+                    for ($i = $currentPage + 1; $i < $currentPage + 3 && $i != $countUsers + 1; $i++) {
+                        $routes = $routes . "<a href='?page=$i'>$i</a>";
+                    }
+
+                    $routes = $currentPage > 3 ? "<a class='main__route-left' href='?page=1'>1</a>" . $routes : $routes;
+                    $routes = $currentPage < $countUsers - 2 ? $routes . "<a class='main__route-right' href='?page=$countUsers'>$countUsers</a>" : $routes;
+
+                    echo "<div class='main__router'>$routes</div>";
                 } else {
                     echo "<strong>Content is unavailable.<br/>
                             You need to log in as an administrator</strong>";      
